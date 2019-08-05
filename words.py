729f15271825d253091fc34c4570f6ed0871658d
hugo-valle <hugovalle1@weber.edu>
2019-08-05 14:51:31 -0600

    Initial commit

--- words.py
+++ words.py
@@ -7,7 +7,18 @@ Task 1: Count number of words in document
 from urllib.request import urlopen
 file = "http://icarus.cs.weber.edu/~hvalle/hafb/words.txt"
 
+count = 0
+data = {}       # empty dictionary
 with urlopen(file) as story:
     for line in story:
-        print(line)
-        
+        words = line.decode('utf-8').split()  # split with space as separator
+        # print(words)
+        for word in words:
+            # check if key already in dict
+            if word in data:
+                data[word] += 1
+            else:
+                data[word] = 1
+            count += 1
+print("Total number of words", count)
+print("Total data", data)
