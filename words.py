3a6433aeb844ed69b34944c0e8d57e4f3d0b3b24
hugo-valle <hugovalle1@weber.edu>
2019-08-05 16:04:19 -0600

    Added fetch_words and print_items functions.

--- words.py
+++ words.py
@@ -7,33 +7,37 @@ Task 1: Count number of words in document
 from urllib.request import urlopen
 
 
-def fetch_words():
+def fetch_words(filename):
     """
-    Fetch the words from a file on the web
-    :return:
+    Count words in url file
+    :param filename: url to file
+    :return: nothing
     """
-    file = "http://icarus.cs.weber.edu/~hvalle/hafb/words.txt"
     count = 0
-    data = {}       # empty dictionary
-    with urlopen(file) as story:
+    data = []       # empty list
+    with urlopen(filename) as story:
         for line in story:
             words = line.decode('utf-8').split()  # split with space as separator
             # print(words)
             for word in words:
-                # check if key already in dict
-                if word in data:
-                    data[word] += 1
-                else:
-                    data[word] = 1
-                count += 1
-    print("Total number of words", count)
-    # Sort by keys
-    for key in sorted(data.keys()):
-        print(key, data[key])
+                data.append(word)
+    return data
+
+
+def print_items(items):
+    """
+    Print elements of the collection
+    :param items: A collections of objects
+    :return: nothing
+    """
+    for item in items:
+        print(item)
 
 
 def main():
-    fetch_words()
+    file = "http://icarus.cs.weber.edu/~hvalle/hafb/words.txt"
+    words = fetch_words(file)
+    print_items(words)
 
 
 if __name__ == '__main__':
