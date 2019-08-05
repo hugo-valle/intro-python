1a7dc69f4d16f81b36ce7466096411240181f0d6
hugo-valle <hugovalle1@weber.edu>
2019-08-05 11:06:14 -0600

    Add if, elif, else

--- control_flow.py
+++ control_flow.py
@@ -7,4 +7,23 @@ Learn about Control Flow in Python
 if True:
     print("It is true")
 
+if bool("eggs"):
+    print("Yes please!")
+
+if "eggs":
+    print("Yes, why not")
+
+# Flat is better than Nested
+h = 42
+if h > 50:
+    print("Greater than 50")
+    if h > 100:
+        print("Greater than 100")
+elif h < 50: # ElSE IF
+    print("Less than 50")
+else:
+    print("Equals 50")
+
+
+###########
 print("Done")
