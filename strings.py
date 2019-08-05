b694a4d81c14936c488a39230a35ede99c6ef57f
hugo-valle <hugovalle1@weber.edu>
2019-08-05 12:50:59 -0600

    Add capitalize string.

--- strings.py
+++ strings.py
@@ -21,3 +21,16 @@ print(s4)
 # Support for backslash
 s5 = "A\\in a string"
 print(s5)
+s6 = 'this is " wow '
+print(s6)
+
+# Raw strings
+raw_string = r'C:\User\Documents\Books'
+print(raw_string)
+
+# String as sequence
+s = "parrot"
+# index notation: 0, 1, 2, etc
+print("s[4]", s[4], type(s))
+# Capitalize the string
+print(s, s.capitalize())
