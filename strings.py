ca661181965ac584d057a21c803a3fb7ddff267e
hugo-valle <hugovalle1@weber.edu>
2019-08-05 11:32:28 -0600

    Initial commit

---
+++ strings.py
@@ -0,0 +1,23 @@
+"""
+Strings and Collections
+
+A string is a immutable sequence of Unicode code-points
+"""
+print('This is a string')
+print("This is a string too")
+
+# Concatenation and Adjacent Strings
+s1 = "First"
+s2 = "Second"
+print(s1 + s2)
+
+# Multi-line Strings and newlines
+s3 = """This is
+a multi-line
+string"""
+print(s3)
+s4 = "This is\na multi-line\nstring"
+print(s4)
+# Support for backslash
+s5 = "A\\in a string"
+print(s5)
