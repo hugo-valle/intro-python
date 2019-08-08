99b748399a38c2bb46a1301402491c503778253c
hugo-valle <hugovalle1@weber.edu>
2019-08-08 15:09:31 -0600

    Added allocate_seat method

--- airtravel.py
+++ airtravel.py
@@ -30,6 +30,34 @@ class Flight:
     def airline(self):
         return self._number[:2]
 
+    def allocate_seat(self, seat, passenger):
+        """
+        Allocate a seat to a passenger
+        :param seat: A seat designator such as '12C', '21F'
+        :param passenger: The passenger name
+        :return:
+        """
+        rows, seat_letter = self._aircraft.seating_plan()
+
+        letter = seat[-1]   # take the letter from the seat
+        if letter not in seat_letter:
+            raise ValueError("Invalid seat letter {}".format(letter))
+
+        row_text = seat[:-1]
+        try:
+            row = int(row_text)
+        except ValueError:
+            raise ValueError("Invalid seat row{}".format(row_text))
+
+        if row not in rows:
+            raise ValueError("Invalid row number{}".format(row))
+
+        if self._seating[row][letter] is not None:
+            raise ValueError("Seat {} already occupied".format(seat))
+        # Assign the seat
+        self._seating[row][letter] = passenger
+
+
 
 class Aircraft:
 
@@ -50,6 +78,7 @@ class Aircraft:
     def seating_plan(self):
         return(range(1, self._num_rows + 1),
                "ABCDEFGHJK"[:self._num_seats_per_row])
+
 
 
 def main():
--- use_airtravel.py
+++ use_airtravel.py
@@ -15,6 +15,12 @@ def main():
                   "Airbus A319",
                   num_rows=22,
                   num_seats_per_row=6))
+    # pp(f1._seating)
+    f1.allocate_seat("02A", "Guido Van Rossum")  # python
+    f1.allocate_seat("12B", "Rasmus Lerdorf")   # php
+    f1.allocate_seat("15F", "Bjare Stroustrup")  #C++
+    f1.allocate_seat("03A", "Larry Wall") # perl
+    f1.allocate_seat("16F", "Yukihiro Matsumoto") # ruby
     pp(f1._seating)
 
 
