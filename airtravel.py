a71706def2b037e5f7fdc2e30923276e79f4b2f1
hugo-valle <hugovalle1@weber.edu>
2019-08-08 14:35:26 -0600

    Added Aircraft class

--- airtravel.py
+++ airtravel.py
@@ -7,7 +7,7 @@ class Flight:
     """
     A Flight with a particular passenger aircraft
     """
-    def __init__(self, number):
+    def __init__(self, number, aircraft):
         if len(number) != 5:
             raise ValueError("Invalid flight number length {}" .format(number))
         if not number[:2].isalpha():
@@ -16,7 +16,13 @@ class Flight:
             raise ValueError("Invalid airline code {}" .format(number))
         if not number[2:].isdigit():
             raise ValueError("Invalid route code {}" .format(number))
+
         self._number = number # implementation details begin with '_'
+        self._aircraft = aircraft
+
+        rows, seats = self._aircraft.seating_plan()
+        self._seating = [None] + \
+                        [{letter:None for letter in seats} for _ in rows]
 
     def number(self):
         return self._number[2:]
@@ -34,11 +40,16 @@ class Aircraft:
         self._num_rows = num_rows
         self._num_seats_per_row = num_seats_per_row
 
+
     def registration(self):
         return self._registration
 
     def model(self):
         return self._model
+
+    def seating_plan(self):
+        return(range(1, self._num_rows + 1),
+               "ABCDEFGHJK"[:self._num_seats_per_row])
 
 
 def main():
--- use_airtravel.py
+++ use_airtravel.py
@@ -2,6 +2,7 @@
 Use flight class
 """
 from airtravel import Flight, Aircraft
+from pprint import pprint as pp
 
 
 def main():
@@ -9,17 +10,12 @@ def main():
     Test function
     :return:
     """
-    f1 = Flight("SN066")
-    print(f1.number(), f1.airline())
-    # f2 = Flight("ab345")
-    f2 = Flight("AB352")
-    print(f2.number(), f2.airline())
-    # Could use: Flight.number(f)
-    a1 = Aircraft("G-EUP",
+    f1 = Flight("SN066",
+                Aircraft("G-EUP",
                   "Airbus A319",
                   num_rows=22,
-                  num_seats_per_row=6)
-    print(a1.registration(), a1.model())
+                  num_seats_per_row=6))
+    pp(f1._seating)
 
 
 if __name__ == '__main__':
